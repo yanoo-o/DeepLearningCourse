1205f98aa472b3e78df6ba8fa7daba9b58e37a58
Mariano Nino Oliva <maroliva12.mno@gmail.com>
2022-03-14 23:01:14 +0800

    commit on master

--- dl_start.py
+++ dl_start.py
@@ -1,2 +1,22 @@
-print("Hello World")
-print("Initialize DL Course")
+print("Hello World! \n")
+print("Initialize DL Course \n")
+
+
+#Basic python functions
+#Printing random numbers
+import random 
+
+x = [random.randint(0,100) for i in range(10)]
+print(x)
+print("Min", min(x)) 
+print("Max", max(x))
+
+x.sort()
+print("Sorted", x)
+
+#Data type casting
+y = [random.random()*100 for i in range(10)]
+print(y)
+
+y = [int(i) for i in y ]
+print(y)
